bd07965145ecaf52a78f0c6159e22f536e6a5555
Rachel Popa <rpopa@saskpower.com>
2015-12-16 12:43:12 -0600

    Return a message notifying user that a scanner was not found.  Attempt to connect until possible.

--- src/ios/TSLInterface.m
+++ src/ios/TSLInterface.m
@@ -30,7 +30,7 @@
     //Number of tags read in a scan
     NSInteger _transpondersSeen;
     //String to hold tag data to be returned to main app
-    NSString* _partialResultMessage;
+    NSString* _resultMessage;
 }
 
 @end
@@ -39,66 +39,76 @@
 
 -(void)pair:(CDVInvokedUrlCommand*)command
 {
-    _pluginResult = nil;
-    _command = command;
-    _partialResultMessage = @"";
-    _transpondersSeen = 0;
-    _rfidScanners = @[@"1128"];
-    
-    NSLog(@"Entered pair functions!");
-    
-    // Create the TSLAsciiCommander used to communicate with the TSL Reader
-    _commander = [[TSLAsciiCommander alloc] init];
-    _accessoryList = [[EAAccessoryManager sharedAccessoryManager] connectedAccessories];
-    _chosenDeviceIndex = [self getGunIndex];
-    
-    if(_chosenDeviceIndex != -1) {
-        
-        NSLog(@"Accessory list has %ld items", _accessoryList.count);
-        
-        // Connect to the chosen TSL Reader
-        _rfidGun = _accessoryList[_chosenDeviceIndex];
-        
+    [self.commandDelegate runInBackground:^{
+        _pluginResult = nil;
+        _command = command;
+        _resultMessage = @"";
+        _transpondersSeen = 0;
+        _rfidScanners = @[@"1128"];
+
+        // Create the TSLAsciiCommander used to communicate with the TSL Reader
+        _commander = [[TSLAsciiCommander alloc] init];
+        _accessoryList = [[EAAccessoryManager sharedAccessoryManager] connectedAccessories];
+            
         do
         {
             // Disconnect from the current reader, if any
             [_commander disconnect];
-            [_commander connect:_rfidGun];
+            
+            _accessoryList = [[EAAccessoryManager sharedAccessoryManager] connectedAccessories];
+            NSLog(@"Accessory list has %ld items", _accessoryList.count);
+            
+            //Find the first RFID scanner in the array
+            _chosenDeviceIndex = [self getGunIndex];
+            
+            if(_chosenDeviceIndex != -1)
+            {
+                // Connect to the chosen TSL Reader
+                _rfidGun = _accessoryList[_chosenDeviceIndex];
+                [_commander connect:_rfidGun];
+            }
+            else{
+                _resultMessage = @"Scanner not found.";
+                NSLog(@"Scanner not found.");
+                _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:_resultMessage];
+                [_pluginResult setKeepCallbackAsBool:YES];
+                [self.commandDelegate sendPluginResult:_pluginResult callbackId:_command.callbackId];
+                _resultMessage = @"";
+                [NSThread sleepForTimeInterval:2.0f];
+            }
         }while(!_commander.isConnected);
-        
+
         // Issue commands to the reader
         NSLog(@"Commander is connected!");
-        
+
         // Add a logger to the commander to output all reader responses to the log file
         [_commander addResponder:[[TSLLoggerResponder alloc] init]];
-        
+
         // Some synchronous commands will be used in the app
         [_commander addSynchronousResponder];
-        
-        // Performing an inventory could potentially take a long time if many transponders are in range so it is best to handle responses asynchronously
-        //
+
         // The TSLInventoryCommand is a TSLAsciiResponder for inventory responses and can have a delegate
         // (id<TSLInventoryCommandTransponderReceivedDelegate>) that is informed of each transponder as it is received
-        
+
         // Create a TSLInventoryCommand
         _inventoryResponder = [[TSLInventoryCommand alloc] init];
-        
+
         // Add self as the transponder delegate
         _inventoryResponder.transponderReceivedDelegate = self;
-        
+
         // Pulling the Reader trigger will generate inventory responses that are not from the library.
         // To ensure these are also seen requires explicitly requesting handling of non-library command responses
         _inventoryResponder.captureNonLibraryResponses = YES;
-        
+
         // Add the inventory responder to the commander's responder chain
         [_commander addResponder:_inventoryResponder];
-        
+
         // Ensure the reader is in a known (default) state
         // No information is returned by the reset command other than its succesful completion
         TSLFactoryDefaultsCommand * resetCommand = [TSLFactoryDefaultsCommand synchronousCommand];
-        
+
         [_commander executeCommand:resetCommand];
-        
+
         // Notify user device has been reset
         if( resetCommand.isSuccessful )
         {
@@ -108,27 +118,28 @@
         {
             NSLog(@"!!! Unable to reset reader to Factory Defaults !!!\n");
         }
-        
+
         // Get version information for the reader
         // Use the TSLVersionInformationCommand synchronously as the returned information is needed below
         TSLVersionInformationCommand * versionCommand = [TSLVersionInformationCommand synchronousCommand];
-        
+
         [_commander executeCommand:versionCommand];
-        
+
         // Log some of the values obtained
         NSLog( @"\n%-16s %@\n%-16s %@\n%-16s %@\n\n\n",
               "Manufacturer:", versionCommand.manufacturer,
               "Serial Number:", versionCommand.serialNumber,
               "Antenna SN:", versionCommand.antennaSerialNumber
               );
-        
-        
-        _partialResultMessage = [NSString stringWithFormat:@"%@ is connected.", versionCommand.serialNumber];
-        _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:_partialResultMessage];
+
+
+        _resultMessage = [NSString stringWithFormat:@"%@ is connected.", versionCommand.serialNumber];
+        _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:_resultMessage];
         [_pluginResult setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:_pluginResult callbackId:_command.callbackId];
-        _partialResultMessage = @"";
-    }
+        _resultMessage = @"";
+            
+    }];
 }
 
 -(NSInteger)getGunIndex
@@ -155,19 +166,19 @@
     //_partialResultMessage = [_partialResultMessage stringByAppendingFormat:@"%-28s  %4d\n", [epc UTF8String], [rssi intValue]];
     
     // _partialResultMessage = @"";
-    _partialResultMessage = [_partialResultMessage stringByAppendingFormat:@"%-24s", [epc UTF8String]];
+    _resultMessage = [_resultMessage stringByAppendingFormat:@"%-24s", [epc UTF8String]];
     
     if (moreAvailable) {
-        _partialResultMessage = [_partialResultMessage stringByAppendingFormat:@"--"];
+        _resultMessage = [_resultMessage stringByAppendingFormat:@"--"];
     }
     _transpondersSeen++;
     
     // If this is the last transponder send the results back to the app
     if( !moreAvailable )
     {
-        NSLog(@"Result Message: %@", _partialResultMessage);
-        if (_partialResultMessage != nil && [_partialResultMessage length] > 0) {
-            _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:_partialResultMessage];
+        NSLog(@"Result Message: %@", _resultMessage);
+        if (_resultMessage != nil && [_resultMessage length] > 0) {
+            _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:_resultMessage];
         } else {
             _pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
         }
@@ -176,7 +187,7 @@
         [self.commandDelegate sendPluginResult:_pluginResult callbackId:_command.callbackId];
         
         _transpondersSeen = 0;
-        _partialResultMessage = @"";
+        _resultMessage = @"";
     }
 }
 
@@ -186,12 +197,14 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     [_commander disconnect];
+    NSLog(@"Application became inactive");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Attempt to reconnect to the last used accessory
     [_commander connect:nil];
+    NSLog(@"Application became active");
 }
 
 
